e42160ac700ad2718c2432d7c4338eed7af05757
you <you@example.com>
2024-11-11 09:39:17 +0530

    manager model

--- MainProject/accounts/models.py
+++ MainProject/accounts/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class MyManager(models.Manager):
+    def my_query(self):
+        return self.all()
+
+
+
 # Create your models here.
 class Address(models.Model):
+    objects=MyManager()
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     title=models.CharField(max_length=100)
     address_line1=models.CharField(max_length=100)

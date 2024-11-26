dd16a5022232e96f56ac82d1c45b093571c18bea
you <you@example.com>
2024-11-26 09:16:29 +0530

    payment complete

--- MainProject/order/views.py
+++ MainProject/order/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from accounts.models import Address
-from django.conf  import settings
+from django.conf import settings
+from accounts.models import User,Customer,Address
+from cart.models import Cart
+from order.models import Order,OrderDetails
 import razorpay
-
-
-
 # @login_required(login_url='/accounts/login/')
 class Checkout(View):
     # @login_required
@@ -23,13 +23,39 @@ class Checkout(View):
             request.session['address_id'] = address_id
             return redirect('proceed_to_pay')
         else:
-            return redirect('checkout')
+            return redirect('ceckout')
         
         
 def proceed_to_pay(request):
-    RAZORPAY_KEY_ID=settings.RAZORPAY_KEY_ID
-    RAZORPAY_KEY_SECRET=settings.RAZORPAY_KEY_SECRET
-    print(RAZORPAY_KEY_ID,RAZORPAY_KEY_SECRET)
-    razorpay.Client()
-
-    return render(request,'order/proceed_to_pay.html')
+    RAZORPAY_KEY_ID = settings.RAZORPAY_KEY_ID
+    RAZORPAY_KEY_SECRET = settings.RAZORPAY_KEY_SECRET
+    client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
+    user = User.objects.get(username = request.user)
+    cart_item = Cart.objects.filter(user = request.user)
+    total = 0
+    order = Order.objects.filter(user = user).filter(status='CREATED').first()
+    if not order:
+        order = Order.objects.create(
+            user = user,
+            status = 'CREATED',
+            total  = total,
+            shipping_address = Address.objects.get(id=int(request.session['address_id'])),
+            shipping_charges = 100
+        )
+    
+    for item in cart_item:
+        total += item.product.price_inclusive * item.quantity
+        OrderDetails.objects.create(order_id=order,
+                                    product=item.product,
+                                    quantity=item.quantity,
+                                    price=item.product.price_inclusive )
+    order.total = total
+    order.save()
+    data = { "amount": int(total), "currency": "INR", "receipt": str(order.order_uuid) }
+    razor_pay_order = client.order.create(data=data)
+    context = {
+        'order':razor_pay_order,
+        'data':data,
+        "RAZORPAY_KEY_ID":RAZORPAY_KEY_ID
+    }
+    return render(request,'order/proceed_to_pay.html',context)
